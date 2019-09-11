54e7ac058e5d40d9e06e8dc3e06224af15ccc07a
Eero Linna <eero.linna@gmail.com>
2019-09-11 15:37:43 +0300

    Fix python test
    The test had a wrong path to the posterior database directory. This essentially
    caused the tests to not be run. The improved test contains a check to prevent
    this from happening in the future.

--- python/tests/test_pdb.py
+++ python/tests/test_pdb.py
@@ -5,14 +5,16 @@ from posteriordb import PosteriorDatabase
 
 
 def test_posterior_database():
-    # path to ../.. which is the posterior database directory
-    path = os.path.dirname(os.path.dirname(__file__))
-    pdb = PosteriorDatabase(path)  # needs to get parent directory I think
+    # path to ../../ which is the posterior database directory
+    path = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+    pdb = PosteriorDatabase(path)
 
     posterior_names = pdb.posterior_names()
+
+    assert len(posterior_names) > 0
 
     for name in posterior_names:
         posterior = Posterior(name, pdb)
 
-        assert posterior.data()
-        assert posterior.model_code("stan")
+        assert posterior.dataset() is not None
+        assert posterior.model_code("stan") is not None
