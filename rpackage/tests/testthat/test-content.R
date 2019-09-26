81e1a37390fab3e4ab65c621e6ec4a70357a4ff1
Paul Buerkner <paul.buerkner@gmail.com>
2019-09-26 16:05:58 +0300

    fix tests

--- rpackage/tests/testthat/test-content.R
+++ rpackage/tests/testthat/test-content.R
@@ -2,7 +2,7 @@ context("test-pdb-content")
 
 test_that("Check that all posteriors can access stan_data and stan_code", {
   expect_silent(pdb_dir <- posteriordb:::get_pdb_dir(getwd()))
-  expect_silent(pdb_test <- pdb(x = pdb_dir))
+  expect_silent(pdb_test <- pdb(pdb_dir))
   expect_silent(posteriors <- posterior_names(pdb_test))
 
   for (i in seq_along(posteriors)) {
