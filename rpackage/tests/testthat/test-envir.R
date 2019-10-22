79696e2ae0aa8f72c44f7fe742aa4bfd0f2612a9
Måns Magnusson <mons.magnusson@gmail.com>
2019-10-22 16:55:33 +0300

    Can I access Travis Build DIR?

--- rpackage/tests/testthat/test-envir.R
+++ rpackage/tests/testthat/test-envir.R
@@ -1,9 +1,9 @@
 context("test-envir-travis")
 
-test_that("Environmental variable POSTERIOR_DB_PATH exist at Travis", {
+test_that("Environmental variable TRAVIS_BUILD_DIR exist on Travis CI", {
   on_travis <- identical(Sys.getenv("TRAVIS"), "true")
   if(on_travis){
-    expect_failure(expect_equal(Sys.getenv("POSTERIOR_DB_PATH"), expected = ""))
+    expect_failure(expect_equal(Sys.getenv("TRAVIS_BUILD_DIR"), expected = ""))
   } else {
     expect_true(TRUE) # To give a nice test pass
   }
