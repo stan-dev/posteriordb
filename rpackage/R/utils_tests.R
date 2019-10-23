f8998a52fa1131d912e1317d9bcebff023126aa3
Eero Linna <eero.linna@gmail.com>
2019-10-23 13:36:30 +0300

    Fix trailing whitespace

--- rpackage/R/utils_tests.R
+++ rpackage/R/utils_tests.R
@@ -4,12 +4,12 @@
 #' Get path to the posterior database
 #' @noRd
 get_test_pdb_dir <- function(x) {
-  
+
   # If on Travis - use Travis build path
   # To handle covr::codecov, that test package in temp folder
   on_travis <- identical(Sys.getenv("TRAVIS"), "true")
   x <- getwd()
   if (on_travis) x <- Sys.getenv("TRAVIS_BUILD_DIR")
-  
+
   pdb_dir_entrypoint(x)
 }
