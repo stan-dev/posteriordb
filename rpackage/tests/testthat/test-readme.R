f200542e328048e6d82dc4d0075f79f619ee9831
Eero Linna <eero.linna@gmail.com>
2019-11-20 17:34:41 +0200

    Make new test work in travis too

--- rpackage/tests/testthat/test-readme.R
+++ rpackage/tests/testthat/test-readme.R
@@ -2,8 +2,8 @@
 context("test-readme")
 
 test_that("Check that the generated README is up-to-date", {
-  current_readme <- readLines("../../../README.md")
-  rmarkdown::render("../../../README.Rmd")
-  generated_readme <- readLines("../../../README.md")
+  current_readme <- readLines("../../../../README.md")
+  rmarkdown::render("../../../../README.Rmd")
+  generated_readme <- readLines("../../../../README.md")
   expect_equal(current_readme, generated_readme)
 })
