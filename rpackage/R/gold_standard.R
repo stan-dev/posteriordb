bfbba1c24cc4deeb49dc82a12bd80efd34204212
Måns Magnusson <mons.magnusson@gmail.com>
2020-01-08 12:58:15 +0200

    Changed gold standard -> reference posterior

--- rpackage/R/gold_standard.R
+++ rpackage/R/gold_standard.R
@@ -1,128 +1,127 @@
-#' Gold standard information
+#' Reference posterior information
 #'
 #' @details
-#' Gold standard information contain information on how, when and by whom
-#' the gold standard was computed.
+#' Reference posterior information on how, when and by whom
+#' the reference posterior was computed.
 #'
-#'
-#' @param x a \code{posterior} object to access the gold standard for.
+#' @param x a \code{posterior} object to access the reference posterior for.
 #' @param pdb a \code{pdb} posterior database connection.
 #' @param ... Currently not used.
 #'
-#' @seealso gold_standard_draws
+#' @seealso reference_posterior_draws
 #'
 #' @export
-gold_standard_info <- function(x, pdb = pdb_default(), ...) {
-  UseMethod("gold_standard_info")
+reference_posterior_info <- function(x, pdb = pdb_default(), ...) {
+  UseMethod("reference_posterior_info")
 }
 
-#' @rdname gold_standard_info
+#' @rdname reference_posterior_info
 #' @export
-gold_standard_info.pdb_posterior <- function(x, ...) {
-  read_gold_standard_info(x$gold_standard_name, x$pdb)
+reference_posterior_info.pdb_posterior <- function(x, ...) {
+  read_reference_posterior_info(x$reference_posterior_name, x$pdb)
 }
 
-#' @rdname gold_standard_info
+#' @rdname reference_posterior_info
 #' @export
-gold_standard_info.character <- function(x, pdb = pdb_default(), ...) {
-  gold_standard_info(posterior(x, pdb))
+reference_posterior_info.character <- function(x, pdb = pdb_default(), ...) {
+  reference_posterior_info(posterior(x, pdb))
 }
 
 
-#' Read in gold standard information json object
+#' Read in reference_posterior_info json object
 #' @param x a data, model or posterior name
 #' @param pdb a posterior db object to access the info json from
 #' @noRd
 #' @keywords internal
-read_gold_standard_info <- function(x, pdb = NULL, ...) {
-  if(is.null(x)) stop("There is currently no gold standard for this posterior.")
-  gold_standard_info <- read_info_json(x, path = "gold_standards/info", pdb = pdb, ...)
-  class(gold_standard_info) <- "pdb_gold_standard_info"
-  assert_gold_standard_info(gold_standard_info)
-  gold_standard_info
+read_reference_posterior_info <- function(x, pdb = NULL, ...) {
+  if(is.null(x)) stop("There is currently no reference posterior for this posterior.")
+  reference_posterior_info <- read_info_json(x, path = "reference_posterior/info", pdb = pdb, ...)
+  class(reference_posterior_info) <- "pdb_reference_posterior_info"
+  assert_reference_posterior_info(reference_posterior_info)
+  reference_posterior_info
 }
 
 
 
-#' Read gold_standard_draws json object
+#' Read reference_posterior_draws json object
 #' @param x a data, model or posterior name
 #' @param pdb a posterior db object to access the info json from
 #' @param ... further arguments. Currently not used.
 #' @noRd
 #' @keywords internal
-read_gold_standard_draws <- function(x, pdb, ...) {
-  if(is.null(x)) stop("There is currently no gold standard for this posterior.", call. = FALSE)
-  gsfp <- pdb_cached_local_file_path(pdb, file.path("gold_standards", "draws", paste0(x, ".json")), unzip = TRUE)
-  gsd <- jsonlite::read_json(gsfp, simplifyVector = TRUE)
-  gsd$draws <- posterior::as_draws(gsd$draws)
-  class(gsd) <- c("pdb_gold_standard_draws", class(gsd))
-  assert_gold_standard_draws(gsd)
-  gsd
+read_reference_posterior_draws <- function(x, pdb, ...) {
+  if(is.null(x)) stop("There is currently no reference posterior for this posterior.", call. = FALSE)
+  rpfp <- pdb_cached_local_file_path(pdb, file.path("reference_posteriors", "draws", paste0(x, ".json")), unzip = TRUE)
+  rpd <- jsonlite::read_json(rpfp, simplifyVector = TRUE)
+  rpd$draws <- posterior::as_draws(rpd$draws)
+  class(rpd) <- c("pdb_reference_posterior_draws", class(rpd))
+  assert_reference_posterior_draws(rpd)
+  rpd
 }
 
 #' Extract data for posterior
 #'
 #' @inheritParams model_code_file_path
 #' @export
-gold_standard_draws_file_path <- function(x, ...) {
-  UseMethod("gold_standard_draws_file_path")
+reference_posterior_draws_file_path <- function(x, ...) {
+  UseMethod("reference_posterior_draws_file_path")
 }
 
-#' @rdname gold_standard_draws_file_path
+#' @rdname reference_posterior_draws_file_path
 #' @export
-gold_standard_draws_file_path.pdb_posterior <- function(x, ...) {
-  if(is.null(x$gold_standard_name)) stop2("There is currently no gold standard for this posterior.")
-  gold_standard_draws_file_path(x$gold_standard_name, pdb = x$pdb)
+reference_posterior_draws_file_path.pdb_posterior <- function(x, ...) {
+  if(is.null(x$reference_posterior_name)) stop2("There is currently no gold standard for this posterior.")
+  reference_posterior_draws_file_path(x$reference_posterior_name, pdb = x$pdb)
 }
 
-#' @rdname gold_standard_draws_file_path
+#' @rdname reference_posterior_draws_file_path
 #' @export
-gold_standard_draws_file_path.character <- function(x, pdb = pdb_default(), ...) {
+reference_posterior_draws_file_path.character <- function(x, pdb = pdb_default(), ...) {
   checkmate::assert_string(x)
-  fp <- paste0("gold_standards/draws/", x, ".json")
-  gsfp <- pdb_cached_local_file_path(pdb, path = fp, unzip = TRUE)
-  checkmate::assert_file_exists(gsfp)
-  gsfp
+  fp <- paste0("reference_posteriors/draws/", x, ".json")
+  rpfp <- pdb_cached_local_file_path(pdb, path = fp, unzip = TRUE)
+  checkmate::assert_file_exists(rpfp)
+  rpfp
 }
 
-#' @rdname gold_standard_draws_file_path
+#' @rdname reference_posterior_draws_file_path
 #' @export
-gold_standard_draws_file_path.pdb_gold_standard_info <- function(x, pdb = pdb_default(), ...) {
-  gold_standard_draws_file_path(x$name, pdb, ...)
+reference_posterior_draws_file_path.pdb_reference_posterior_info <- function(x, pdb = pdb_default(), ...) {
+  reference_posterior_draws_file_path(x$name, pdb, ...)
 }
 
-#' Gold standard posterior draws
+#' Reference Posterior posterior draws
 #' @param x a [posterior] object or a posterior name.
 #' @param pdb a [pdb] object (if [x] is a posterior name)
 #' @param ... further arguments supplied to specific methods.
-#' @return a gold_standard, draws_list object.
+#' @return a reference posterior, draws_list object.
 #' @export
-gold_standard_draws <- function(x, ...){
-  UseMethod("gold_standard_draws")
+reference_posterior_draws <- function(x, ...){
+  UseMethod("reference_posterior_draws")
 }
 
-#' @rdname gold_standard_draws
+#' @rdname reference_posterior_draws
 #' @export
-gold_standard_draws.character <- function(x, pdb = pdb_default(), ...){
-  gold_standard_draws(posterior(x, pdb = pdb))
+reference_posterior_draws.character <- function(x, pdb = pdb_default(), ...){
+  reference_posterior_draws(posterior(x, pdb = pdb))
 }
 
-#' @rdname gold_standard_draws
+#' @rdname reference_posterior_draws
 #' @export
-gold_standard_draws.pdb_posterior <- function(x, ...){
-  read_gold_standard_draws(x = x$gold_standard_name, pdb = x$pdb)
+reference_posterior_draws.pdb_posterior <- function(x, ...){
+  read_reference_posterior_draws(x = x$reference_posterior_name, pdb = x$pdb)
 }
 
-#' @rdname gold_standard_draws
+#' @rdname reference_posterior_draws
 #' @export
-gold_standard_draws.pdb_gold_standard_info <- function(x, pdb = pdb_default(), ...){
-  read_gold_standard_draws(x = x$name, pdb = pdb)
+reference_posterior_draws.pdb_reference_posterior_info <- function(x, pdb = pdb_default(), ...){
+  read_reference_posterior_draws(x = x$name, pdb = pdb)
 }
 
-#' @rdname gold_standard_draws
+#' @rdname reference_posterior_draws
 #' @export
-assert_gold_standard_draws <- function(x){
-  checkmate::assert_class(x, c("pdb_gold_standard_draws"))
+assert_reference_posterior_draws <- function(x){
+  checkmate::assert_class(x, c("pdb_reference_posterior_draws"))
 
   checkmate::assert_names(names(x), subset.of = c("name", "draws"))
 
@@ -136,23 +135,46 @@ assert_gold_standard_draws <- function(x){
   }
 }
 
-#' Assert a gold_standard info
-#' @param x a an object to assert is a gold_standard_info object.
+#' Assert a [pdb_reference_posterior_info] object
+#' @param x a an object to assert is a [pdb_reference_posterior_info] object.
 #' @keywords internal
-assert_gold_standard_info <- function(x){
-  checkmate::assert_class(x, "pdb_gold_standard_info")
-  checkmate::assert_names(names(x), identical.to = c("name", "inference_method", "inference_method_arguments", "inference_version", "added_by", "added_date"))
+assert_reference_posterior_info <- function(x){
+  checkmate::assert_class(x, "pdb_reference_posterior_info")
+  checkmate::assert_names(names(x), identical.to = c("name", "inference", "diagnostics", "added_by", "added_date", "versions"))
   checkmate::assert_string(x$name)
-  checkmate::assert_true(x$inference_method %in% c("stan_sampling", "analytical"))
-  checkmate::assert_list(x$inference_method_arguments)
+
+  checkmate::assert_true(x$inference$method %in% c("stan_sampling", "analytical"))
+  checkmate::assert_list(x$inference$method_arguments)
+
+  if(!is.null(x$diagnostics)){
+    checkmate::assert_names(names(x$diagnostics),
+                            subset.of = c("effective_sample_size_bulk",
+                                          "effective_sample_size_tail",
+                                          "r_hat",
+                                          "divergent_transitions",
+                                          "effective_sample_size_bulk_per_iteration",
+                                          "effective_sample_size_tail_per_iteration",
+                                          "expected_fraction_of_missing_information"))
+  }
+
+  checkmate::assert_string(x$added_by)
+  checkmate::assert_date(x$added_date)
+  checkmate::assert_names(names(x$versions), subset.of = c("rstan_version", "r_Makevars", "r_version", "r_session"))
+  if(!is.null(x$versions$rstan_version)){
+    checkmate::assert_names(names(x$versions), must.include = c("rstan_version", "r_Makevars", "r_version", "r_session"))
+  }
+  for(i in seq_along(x$versions)){
+    checkmate::assert_string(x$versions[[i]])
+  }
 }
 
-#' Subset a [pdb_gold_standard_draws] object
-#' @param x a [pdb_gold_standard_draws] to subest
+
+#' Subset a [pdb_reference_posterior_draws] object
+#' @param x a [pdb_reference_posterior_draws] to subest
 #' @param variable Parameters to subset.
 #' @param ... Further arguments (not used).
 #' @export
-subset.pdb_gold_standard_draws <- function(x, variable, ...){
+subset.pdb_reference_posterior_draws <- function(x, variable, ...){
   requireNamespace("posterior")
   parameters <- paste0("^", variable, "(\\[[0-9]+\\])?$")
   x$draws <- subset(x$draws, variable = parameters, regex = TRUE)
@@ -161,16 +183,16 @@ subset.pdb_gold_standard_draws <- function(x, variable, ...){
 
 
 #' @export
-print.pdb_gold_standard_draws <- function(x, ...) {
+print.pdb_reference_posterior_draws <- function(x, ...) {
   cat0("Posterior: ", x$name, "\n")
   print(posterior::summarise_draws(x$draws))
 }
 
 
 #' @export
-print.pdb_gold_standard_info <- function(x, ...) {
+print.pdb_reference_posterior_info <- function(x, ...) {
   cat0("Posterior: ", x$name, "\n")
-  cat0("Method: ", x$inference_method, " (", x$inference_version, ")\n")
+  cat0("Method: ", x$inference$method, " (", x$versions[[1]], ")\n")
   cat0("Arguments:\n")
-  print_list(x$inference_method_arguments)
+  print_list(x$inference$method_arguments)
 }
