db6d87b45da1670386a3718c3ddd927c95f0bffb
Måns Magnusson <mons.magnusson@gmail.com>
2020-04-07 21:54:15 +0300

    Added handling of removing stuff from pdb

--- rpackage/R/write_pdb.R
+++ rpackage/R/write_pdb.R
@@ -78,37 +78,3 @@ write_pdb.pdb_posterior <- function(x, pdb,  overwrite = FALSE, ...){
   class(x) <- c(class(x), "list")
   write_json_to_path(x, "posteriors", pdb, zip = FALSE, info = FALSE, overwrite = overwrite)
 }
-
-
-#' Remove objects from local pdb
-#'
-#' @description a function to simplify removing object from a local pdb.
-#'
-#' @param x an object to remove to the pdb.
-#' @param pdb the pdb to remove from. Currently only a local pdb.
-#' @param ... further arguments supplied to methods.
-remove_pdb <- function(x, pdb, ...){
-  checkmate::assert_class(pdb, "pdb_local")
-  UseMethod("remove_pdb")
-}
-
-#' @rdname remove_pdb
-remove_pdb.pdb_data <- function(x, pdb, ...){
-  fn <- paste0(info(x)$name, ".json.zip")
-  fp <- pdb_file_path(pdb, "data", "data", fn)
-  remove_pdb(info(x), pdb = pdb)
-  remove_pdb(fp, pdb)
-}
-
-#' @rdname remove_pdb
-remove_pdb.pdb_data_info <- function(x, pdb, ...){
-  fn <- paste0(x$name, ".info.json")
-  fp <- pdb_file_path(pdb, "data", "info", fn)
-  remove_pdb(fp, pdb)
-}
-
-#' @rdname remove_pdb
-remove_pdb.character <- function(x, pdb, ...){
-  checkmate::assert_file_exists(x)
-  file.remove(x)
-}
