ad253bac85cab776a29d7ce38800f7feeb997295
Måns Magnusson <mons.magnusson@gmail.com>
2020-09-17 16:45:55 +0200

    Minor bug fix

--- rpackage/R/check_posterior.R
+++ rpackage/R/check_posterior.R
@@ -17,10 +17,10 @@ check_posterior <- function(po, run_stan_code_checks = TRUE) {
   check_pdb_read_model_code(list(po))
   message("- The model_code can be read.")
 
-  posteriordb:::check_pdb_read_data(list(po))
+  check_pdb_read_data(list(po))
   message("- The data can be read.")
 
-  posteriordb:::check_pdb_read_reference_posterior_draws(list(po))
+  check_pdb_read_reference_posterior_draws(list(po))
   message("- The reference_posteriors_draws can be read (if it exists).")
 
   check_pdb_aliases(pdb(po))
