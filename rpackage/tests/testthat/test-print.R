e478e5c1a0dbd996f15234b61652cfddfb3e630e
Måns Magnusson <mons.magnusson@gmail.com>
2020-09-18 21:55:10 +0200

    Small fixes

--- rpackage/tests/testthat/test-print.R
+++ rpackage/tests/testthat/test-print.R
@@ -1,7 +1,7 @@
 context("test-print")
 
 test_that("All PPFs are printed", {
-
+  skip_on_appveyor()
   posterior_db_path <- posteriordb:::get_test_pdb_dir()
   expect_silent(pdb_test <- pdb(posterior_db_path))
   expect_silent(po <- posterior("eight_schools-eight_schools_noncentered", pdb = pdb_test))
