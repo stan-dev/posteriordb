95db14fbc8910273a62a20ca0398315aefca2626
Måns Magnusson <mons.magnusson@gmail.com>
2020-09-19 10:01:15 +0200

    Update appveyor tests

--- rpackage/R/utils_tests.R
+++ rpackage/R/utils_tests.R
@@ -4,15 +4,12 @@
 #' Get path to the posterior database
 #' @noRd
 get_test_pdb_dir <- function(x) {
+  x <- getwd()
   # If on Travis - use Travis build path
   # To handle covr::codecov, that test package in temp folder
-  on_travis <- identical(Sys.getenv("TRAVIS"), "true")
+  if (on_travis()) x <- Sys.getenv("TRAVIS_BUILD_DIR")
   # If on Appveyor - use Appveyor build path
-  on_appveyor <- identical(Sys.getenv("APPVEYOR"), "true")
-
-  x <- getwd()
-  if (on_travis) x <- Sys.getenv("TRAVIS_BUILD_DIR")
-  if (on_appveyor) x <- Sys.getenv("APPVEYOR_BUILD_FOLDER")
+  if (on_appveyor()) x <- Sys.getenv("APPVEYOR_BUILD_FOLDER")
   find_local_posterior_database(x)
 }
 
@@ -34,3 +31,6 @@ find_local_posterior_database <- function(x){
     stop2("No local posterior database in path '", fpep, "'.")
   }
 }
+
+on_travis <- function() identical(Sys.getenv("TRAVIS"), "true")
+on_appveyor <- function() identical(Sys.getenv("APPVEYOR"), "true")
