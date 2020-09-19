246dea41b9248fba4191560624ebc98a61c36930
Måns Magnusson <mons.magnusson@gmail.com>
2020-09-19 11:58:07 +0200

    small additional test

--- rpackage/tests/testthat/test-check_posterior_draws.R
+++ rpackage/tests/testthat/test-check_posterior_draws.R
@@ -13,3 +13,11 @@ test_that("test-check_posterior_draws", {
   expect_equal(rp1, rp2)
   expect_equal(rp1, rp3)
 })
+
+test_that("check_pdb_posterior works", {
+  posterior_db_path <- posteriordb:::get_test_pdb_dir()
+  expect_silent(pdb_test <- pdb(posterior_db_path))
+
+  po <- pdb_posterior("eight_schools-eight_schools_noncentered", pdb_test)
+  expect_message(check_pdb_posterior(po, run_stan_code_checks = FALSE), regexp = "Posterior is ok.")
+})
