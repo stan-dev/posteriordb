585bbc640d2258a100f116d1addc88057880b775
ahartikainen <ahartikainen@github.com>
2020-11-05 14:13:40 +0200

    add __version__ to python

--- python/setup.py
+++ python/setup.py
@@ -1,8 +1,22 @@
+import os
+import re
 from setuptools import setup
+
+PROJECT_ROOT = os.path.dirname(os.path.realpath(__file__))
+VERSION_FILE = os.path.join(PROJECT_ROOT, "src", "posteriordb", "__init__.py")
+
+def get_version():
+    lines = open(VERSION_FILE, "rt").readlines()
+    version_regex = r"^__version__ = ['\"]([^'\"]*)['\"]"
+    for line in lines:
+        mo = re.search(version_regex, line, re.M)
+        if mo:
+            return mo.group(1)
+    raise RuntimeError("Unable to find version in %s." % (VERSION_FILE,))
 
 setup(
     name="posteriordb",
-    version="0.1",
+    version=get_version(),
     description="python interface to posterior database",
     url="https://github.com/MansMeg/posteriordb",
     author="Eero Linna",
