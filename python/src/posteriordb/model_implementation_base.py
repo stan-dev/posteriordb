90bd1b28dac112e42fd567b9967b41f1dc87dd52
Måns Magnusson <mons.magnusson@gmail.com>
2021-09-06 15:34:13 +0200

    Move R package and python lib to separate repos

--- python/src/posteriordb/model_implementation_base.py
+++
@@ -1,7 +0,0 @@
-from abc import ABC, abstractmethod
-
-
-class ModelImplementationBase(ABC):
-    @abstractmethod
-    def load(self):
-        pass
--- python/src/posteriordb/util.py
+++
@@ -1,7 +0,0 @@
-def drop_keys(dct, keys_to_drop):
-    if not isinstance(keys_to_drop, list):
-        keys_to_drop = [keys_to_drop]
-
-    new_dct = {k: v for (k, v) in dct.items() if k not in keys_to_drop}
-
-    return new_dct
