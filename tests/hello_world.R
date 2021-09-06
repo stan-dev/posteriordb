b798993ae2d0e8223c0e33f182c6da9c29a705b4
Måns Magnusson <mons.magnusson@gmail.com>
2021-09-06 17:09:43 +0200

    Test hello world posteriordb

--- tests/hello_world.R
+++ tests/hello_world.R
@@ -1,2 +1,7 @@
+# remotes::install_github("stan-dev/posteriordb-r")
+library(posteriordb)
+pdbl <- pdb_local("posterior_database")
+check_pdb(pdbl, run_stan_code_checks = FALSE)
+
 print("hello world!")
 message("hello world!")
