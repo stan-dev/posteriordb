d271c4f2be33884435f359f9310854d22c51c612
JTorgander <55882444+JTorgander@users.noreply.github.com>
2026-01-16 11:31:32 +0100

    Removing data dependence and sampling from model object

--- posterior_database/models/pymc/earnings-earn_height.py
+++ posterior_database/models/pymc/earnings-earn_height.py
@@ -1,37 +1,23 @@
-
+import numpy as np
 import pymc as pm
-import pandas as pd
-from zipfile import ZipFile
-import json 
 
-# Load data
-zip_file = ZipFile('posterior_database/data/data/earnings.json.zip', 'r')
-data = json.loads(zip_file.read('earnings.json'))
-N = data.pop('N')
-df = pd.DataFrame(data)
+def model(data):
 
-sampler_config = {
-    "chains": 10,
-    "draws": 2000,
-    "tune": 1000,
-    "random_seed": 8675309,
-}
-
-
-# Define PyMC Model
-coords = {
-    'obs_idx': df.index,
-    'feature': ['intercept', 'height']
-}
-
-with pm.Model(coords=coords) as earn_height:
-    earn = pm.Data('earn', df['earn'], dims=['obs_idx'])
-    height = pm.Data('height', df['height'], dims=['obs_idx'])
+    # Define PyMC Model
+    coords = {
+        'obs_idx': np.arange(data["N"]),
+        'feature': ['intercept', 'height']
+    }
     
-    beta = pm.Flat('beta', dims=['feature'])
-    mu = beta[0] + beta[1] * height
-    
-    sigma = pm.HalfFlat('sigma')
-    earn_hat = pm.Normal('earn_hat', mu=mu, sigma=sigma, observed=earn, dims=['obs_idx'])
+    with pm.Model(coords=coords) as earn_height:
+        N = pm.Data("N", data["N"])
+        earn = pm.Data('earn', data['earn'], dims=['obs_idx'])
+        height = pm.Data('height', data['height'], dims=['obs_idx'])
+        
+        beta = pm.Flat('beta', dims=['feature'])
+        mu = beta[0] + beta[1] * height
+        
+        sigma = pm.HalfFlat('sigma')
+        earn_hat = pm.Normal('earn_hat', mu=mu, sigma=sigma, observed=earn, dims=['obs_idx'])
 
-    idata = pm.sample(**sampler_config)
+    return earn_height
