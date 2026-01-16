473d1bdf564e0fd99ca1e4944f54b6ea74d4b665
JTorgander <55882444+JTorgander@users.noreply.github.com>
2026-01-16 13:25:22 +0100

    Updating prior definition

--- posterior_database/models/pymc/eight_schools_noncentered.py
+++ posterior_database/models/pymc/eight_schools_noncentered.py
@@ -11,7 +11,7 @@ def model(data):
         mu = pm.Normal(
             "mu", mu=0, sigma=5
         )  # hyper-parameter of mean, non-informative prior
-        tau = pm.Cauchy("tau", alpha=0, beta=5)  # hyper-parameter of sigma
+        tau = pm.HalfCauchy("tau", beta=5)  # hyper-parameter of sigma
         theta_trans = pm.Normal("theta_trans", mu=0, sigma=1, dims="school")
         theta = mu + tau * theta_trans
         y = pm.Normal("y", mu=theta, sigma=sigma, observed=y_obs)
