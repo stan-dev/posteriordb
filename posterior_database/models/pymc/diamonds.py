0bb1a22d2eec8790ae54e1a75c8b10b614de4f44
JTorgander <55882444+JTorgander@users.noreply.github.com>
2026-08-14 14:34:28 +0200

    Adding data mean centering and regression terms to diamonds model

--- posterior_database/models/pymc/diamonds.py
+++ posterior_database/models/pymc/diamonds.py
@@ -7,9 +7,11 @@ def make_model(data: dict, prior_only: bool = False) -> pm.Model:
     N = data['N']
     Y = data['Y']
     K = data['K'] 
-    X = data['X']
+    X = np.asarray(data['X'])
     
     Kc = K - 1
+    means_X = X[:, 1:].mean(axis=0)
+    Xc = X[:, 1:] - means_X
     
     with pm.Model() as model:
         b = pm.Normal("b", mu=0, sigma=1, shape=Kc)
@@ -17,6 +19,7 @@ def make_model(data: dict, prior_only: bool = False) -> pm.Model:
         sigma = pm.HalfStudentT("sigma", nu=3, sigma=10)
         
         if not prior_only:
-            Y_obs = pm.Normal("Y", mu=Intercept, sigma=sigma, observed=Y)
+            mu = Intercept + pm.math.dot(Xc, b)
+            Y_obs = pm.Normal("Y", mu=mu, sigma=sigma, observed=Y)
 
     return model
